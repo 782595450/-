9399eca6e4e7cccd65444dde0d56512f49e5bac4
赖双全 <782595450@qq.com>
2018-06-20 17:52:09 +0800

    update

---
+++ 大千世界/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  大千世界
+//
+//  Created by 赖双全 on 2018/6/20.
+//  Copyright © 2018年 赖双全. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
